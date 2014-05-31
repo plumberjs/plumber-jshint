62c2b7f605b01fca954d67816988821693a179eb
Oliver Joseph Ash <oliverjash@icloud.com>
2014-05-31 17:44:42 +0100

    Load `.jshintrc` files

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var operation = require('plumber').operation;
 var Report = require('plumber').Report;
+var RcLoader = require('rcloader');
+var highland = require('highland');
 
 var jshint = require('jshint').JSHINT;
 
@@ -14,26 +16,43 @@ function identity(x) {
 
 
 module.exports = function() {
-    return operation.map(function(resource) {
-        // TODO: if necessary, expose as operation parameters
-        var options = {};
-        var globals;
+    return operation(function(resources) {
+        return resources.flatMap(function(resource) {
+            var rcLoader = new RcLoader('.jshintrc', {}, {
+                loader: function (path) {
+                    var jshintcli = require('jshint/src/cli');
+                    var config = jshintcli.loadConfig(path);
+                    // Redundant property added by JSHint
+                    delete config.dirname;
+                    return config;
+                }
+            });
 
-        // FIXME: jshint not concurrency-safe? use serial map?
-        var success = jshint(resource.data(), options, globals);
-        return createReport({
-            resource: resource,
-            type: 'test',
-            success: success,
-            // for some reason, sometimes get `null' in the list
-            errors: jshint.errors.filter(identity).map(function(error) {
-                return {
-                    line: error.line,
-                    column: error.character,
-                    message: error.code + ': ' + error.raw,
-                    context: error.evidence
-                };
-            })
+            var loadRcConfigFor = highland.wrapCallback(rcLoader.for);
+            return loadRcConfigFor(resource.path().absolute()).map(function (rcConfig) {
+                var globals;
+                if (rcConfig.globals) {
+                  globals = rcConfig.globals;
+                  delete rcConfig.globals;
+                }
+
+                // FIXME: jshint not concurrency-safe? use serial map?
+                var success = jshint(resource.data(), rcConfig, globals);
+                return createReport({
+                    resource: resource,
+                    type: 'test',
+                    success: success,
+                    // for some reason, sometimes get `null' in the list
+                    errors: jshint.errors.filter(identity).map(function(error) {
+                        return {
+                            line: error.line,
+                            column: error.character,
+                            message: error.code + ': ' + error.raw,
+                            context: error.evidence
+                        };
+                    })
+                });
+            });
         });
     });
 };
