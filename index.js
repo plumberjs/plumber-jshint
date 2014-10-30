5129b1981256d419729975b5b36128edfface7a3
Oliver Joseph Ash <oliverjash@icloud.com>
2014-10-30 04:22:09 +0100

    Use new `jshintcli.getConfig` instead of manual traversal

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var plumber = require('plumber');
 var operation = plumber.operation;
 var Report = plumber.Report;
-var RcLoader = require('rcloader');
 
 var jshint = require('jshint').JSHINT;
 
@@ -17,43 +16,32 @@ function identity(x) {
 var jshintcli = require('jshint/src/cli');
 
 module.exports = function() {
-    return operation(function(resources) {
-        return resources.flatMap(function(resource) {
-            var rcLoader = new RcLoader('.jshintrc', {}, {
-                loader: function (path) {
-                    // TODO: Make errors happen in Rx context. How?
-                    var config = jshintcli.loadConfig(path);
-                    // Redundant property added by JSHint
-                    delete config.dirname;
-                    return config;
-                }
-            });
+    return operation.map(function(resource) {
+        var config = jshintcli.getConfig(resource.path().absolute());
+        // Redundant property added by JSHint
+        delete config.dirname;
 
-            var loadRcConfigFor = plumber.Rx.Observable.fromNodeCallback(rcLoader.for);
-            return loadRcConfigFor(resource.path().absolute()).map(function (rcConfig) {
-                var globals;
-                if (rcConfig.globals) {
-                  globals = rcConfig.globals;
-                  delete rcConfig.globals;
-                }
+        var globals;
+        if (config.globals) {
+          globals = config.globals;
+          delete config.globals;
+        }
 
-                // FIXME: jshint not concurrency-safe? use serial map?
-                var success = jshint(resource.data(), rcConfig, globals);
-                return createReport({
-                    resource: resource,
-                    type: 'test',
-                    success: success,
-                    // for some reason, sometimes get `null' in the list
-                    errors: jshint.errors.filter(identity).map(function(error) {
-                        return {
-                            line: error.line,
-                            column: error.character,
-                            message: error.code + ': ' + error.raw,
-                            context: error.evidence
-                        };
-                    })
-                });
-            });
+        // FIXME: jshint not concurrency-safe? use serial map?
+        var success = jshint(resource.data(), config, globals);
+        return createReport({
+            resource: resource,
+            type: 'test',
+            success: success,
+            // for some reason, sometimes get `null' in the list
+            errors: jshint.errors.filter(identity).map(function(error) {
+                return {
+                    line: error.line,
+                    column: error.character,
+                    message: error.code + ': ' + error.raw,
+                    context: error.evidence
+                };
+            })
         });
     });
 };
